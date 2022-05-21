d1f2a5fe878a9745703d02e1bc57b6962231b465
Rylan Chin <rchin@ualberta.ca>
2022-05-21 02:00:36 -0600

    changed link formatting for clarity

--- src/components/ProjectTemplates/Projects.jsx
+++ src/components/ProjectTemplates/Projects.jsx
@@ -16,23 +16,25 @@ export default function Projects() {
             <div className="container">
                 {projects.map((proj) => (
                     <div key={proj.id}>
-                        <Link to={`/Projects/${proj.id}`}>
-                            <div className='row align-items-center my-5'>
-                                <div className='col-lg-7'>
+                        <div className='row align-items-center my-5'>
+                            <div className='col-lg-7'>
+                                <Link to={`/Projects/${proj.id}`}>
                                     <img
                                         className='img-fluid rounded mb-4 mb-lg-0'
                                         src={proj.image}
                                         alt=""
                                     />
-                                </div>
-                                <div className='col-lg-5'>
-                                    <h1 className='font-weight-light'>{proj.name}</h1>
-                                    <p>
-                                        {proj.desc}
-                                    </p>
-                                </div>
+                                </Link>
                             </div>
-                        </Link>
+                            <div className='col-lg-5'>
+                                <Link to={`/Projects/${proj.id}`}>
+                                    <h1 className='font-weight-light stealthlink'>{proj.name}</h1>
+                                </Link>
+                                <p>
+                                    {proj.desc}
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
