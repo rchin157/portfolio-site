20b695e6e2c0e0fcc4759dd85e3ac16eaf88a341
Rylan Chin <rchin@ualberta.ca>
2022-09-04 14:03:04 -0600

    updated about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,18 @@
+import profileBanner from "../data/MiscImages/20220822_110747_thin.png";
+
 export default function About() {
     return (
         <div className="home">
+            <div>
+                <img src={profileBanner} alt="About Banner" className="img-fluid" />
+            </div>
             <div className="container">
-                <div className="py-3">
+                <div className="pt-3">
+                    <p>
+                        Hello, my name is Rylan Chin. I'm a student at the University of Alberta in my final year of Computing Science.
+                        I spent the last academic year on internship as a research assistant as part of a team from the faculty of civil engineering.
+                        I'm currently looking for full-time employment in industry this coming spring.
+                    </p>
                     <h5>Location</h5>
                     <p>Edmonton, Alberta, Canada</p>
                     <h5>Contact</h5>
