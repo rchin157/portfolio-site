bcecb40393a251b5c7043f95364f65e49b88aa83
Rylan Chin <rchin@ualberta.ca>
2022-09-05 14:23:10 -0600

    Create secrets.js

---
+++ src/data/secrets.js
@@ -0,0 +1,9 @@
+export const fbauth = {
+    apiKey: "AIzaSyCW6OsrnGr_ZOJIEMC3CxWFv895fChqcL0",
+    authDomain: "portfolio-web-mechs-game.firebaseapp.com",
+    projectId: "portfolio-web-mechs-game",
+    storageBucket: "portfolio-web-mechs-game.appspot.com",
+    messagingSenderId: "204533584499",
+    appId: "1:204533584499:web:b5c227e8c0fb9fa9589006",
+    measurementId: "G-2205CQFVRN",
+}
